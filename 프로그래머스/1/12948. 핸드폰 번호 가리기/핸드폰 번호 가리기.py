99b45acf418b20671b645be7a9daedac158e8e10
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2023-11-06 10:52:52 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -2,4 +2,4 @@ def solution(phone_number):
     answer = ''
     for i in range(len(phone_number)-4) :
         answer += '*'
-    return answer + phone_number[-4:-1] + phone_number[-1]
+    return answer + phone_number[-4:]
